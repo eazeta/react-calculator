ab7d677c56d1e50c62d331a50155281e9831c1e9
eazeta <emmanuel.2000z@gmail.com>
2022-11-26 16:57:02 +0000

    base build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import './styles.css';
+import { useState } from 'react';
+
 function App() {
 	return (
 		<div className='calculator-grid'>
-			<h1>Hi Boss</h1>
+			<div className='output'>
+				<div className=''>{}</div>
+				<div className=''>{}</div>
+			</div>
+			<button>AC</button>
+			<button>DEL</button>
+			<button>+</button>
+			<button>*</button>
+			<button>-</button>
+			<button>/</button>
+			<button>1</button>
+			<button>2</button>
+			<button>3</button>
+			<button>4</button>
+			<button>5</button>
+			<button>6</button>
+			<button>7</button>
+			<button>8</button>
+			<button>9</button>
+			<button>0</button>
+			<button>.</button>
+			<button>=</button>
 		</div>
 	);
 }
